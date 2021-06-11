43c4c40fc8358db319a1e34adb81721e7a47ee83
thiswind <thiswind@gmail.com>
2021-06-11 07:26:09 +0000

    ADD

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# CalculatePi
+# 程序设计实践 - 蒙特卡洛方法计算圆周率
+
+
+## 蒙特卡洛方法
+
+![](bg2015072611.jpg)
+
+![](bg2015072604.jpg)
+
+![](bg2015072603.jpg)
+
+[蒙特卡罗方法入门](http://www.ruanyifeng.com/blog/2015/07/monte-carlo-method.html)
+
+## 实验要求
+
+1. Fork本项目
+2. 设计并编写程序，计算出圆周率，并提交到Github
+
+## 思考题
+
+[calc_pi.py](https://github.com/2021-inclass-example/CalculatePi/blob/main/calc_pi.py) 是一个示例程序，这个程序错在哪里？
--- calc_pi.py
+++ calc_pi.py
@@ -1,67 +1,79 @@
-import sys
 import math
-import random
+import sys
+from random import uniform
 
 
 class Point2D(object):
 
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
-    @classmethod
-    def distance_between(p1, p2):
-        dx = p1.x - p2.x
-        dy = p1.y - p2.y
+    def distance_to(self, other_point) -> float:
+        dx = self.x - other_point.x
+        dy = self.y - other_point.y
 
         distance = math.sqrt(dx * dx + dy * dy)
         return distance
 
+
 class PiCalculator(object):
 
-    def __init__(self, num_points):
-        self.radius = 1
-        self.center = Point(0, 0)
+    def __init__(self, num_points: int):
+        self.radius = 10
+        self.center = Point2D(0, 0)
         self.sum_random_points = num_points
         self.points_list = []
         pass
 
-    def __get_one_random(self):
-        return random() * self.radius * 2 - self.radius
+    def __get_one_random(self) -> float:
+        # uniform(a, b) a~b 之间的一个均匀分布的随机数
+        return uniform(-self.radius, self.radius)
 
     def put_random_points(self, sum_random_points):
-        one_x = self.__get_one_random()
-        one_y = self.__get_one_random()
+        for _ in range(self.sum_random_points):
+            one_x = self.__get_one_random()
+            one_y = self.__get_one_random()
 
-        one_point = Point(x, y)
-        self.points_list.append(one_point)
+            one_point = Point2D(one_x, one_y)
+            self.points_list.append(one_point)
+            self.points_list.append(one_point)
 
-        pass
+    def sum_points_in_circle(self) -> int:
+        sum = 0
 
-    def sum_points_in_circle(self):
-        pass
+        for one_point in self.points_list:
+            distance_to_center = one_point.distance_to(self.center)
+            if distance_to_center <= self.radius:
+                sum += 1
 
-    def get_pi_value(self):
+        return sum
+
+    def get_pi_value(self) -> float:
         """
-        / pi * r * r = S_circle
-        | (r+r) * (r+r) = S_square
-        \ S_circle / S_squre = sum_points_in_circle / sum_random_points
+        S_circle = pi * r**2
+        S_square = (2r) * (2r) = 4 * r**2
+        S_circle / S_square = sum_points_in_circle / sum_random_points
 
-        ==> 
-
-        S_circle = sum_points_in_circle / sum_random_points * S_squre
-        pi * r * r = sum_points_in_circle / sum_random_points * S_squre
-        pi = (sum_points_in_circle / sum_random_points * S_squre) / (r * r)
-        pi = (sum_points_in_circle / sum_random_points * (r+r) * (r+r)) / (r * r)
+        ==> S_circle = (sum_points_in_circle / sum_random_points) * S_square
+        ==> pi * r**2 = (sum_points_in_circle / sum_random_points) * S_square
+        ==> pi = ((sum_points_in_circle / sum_random_points) * S_square) / r**2
+               = ((sum_points_in_circle / sum_random_points) * 4 * r**2) / r**2
         """
-        put_random_points(self.sum_random_points)
-        sum_points_in_circle = sum_points_in_circle()
+        self.put_random_points(self.sum_random_points)
+        sum_points_in_circle = self.sum_points_in_circle()
 
         r = self.radius
 
-        pi = (sum_points_in_circle / sum_random_points * (r + r) * (r+r)) / (r * r)
+        pi = ((sum_points_in_circle / self.sum_random_points) * 4 * r**2) / r**2
 
         return pi
 
+
 if __name__ == '__main__':
+    for i in range(1, 7):
+        n = 1 * 10 ** i
+        caculator = PiCalculator(n)
+        pi = caculator.get_pi_value()
+        print(f'point number {n}, pi {pi}')
     pass
